c20483f2b7d9d9d576c641e0320e967b5e66a179
AhamedJINabil <johayeriqbal@gmail.com>
2022-04-17 05:03:50 +0600

    set env veriable for firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import getAuth from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaiPeF0F7Dd4rCd-6U7Xt_xPPACIEuI-Y",
-  authDomain: "genius-car-services-c8313.firebaseapp.com",
-  projectId: "genius-car-services-c8313",
-  storageBucket: "genius-car-services-c8313.appspot.com",
-  messagingSenderId: "21239205881",
-  appId: "1:21239205881:web:8d7644b9042613fe55f728"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
